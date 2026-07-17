9466e67d30d566d3acf47c25a4e21b42f906d3ec
Andera Singgih Pratama <anderasinggih1@gmail.com>
2026-07-17 21:14:18 +0700

    fix: change limit to 1000000 to bypass ADC Studio min limit validation error

--- custom_dashboard_script.js
+++ custom_dashboard_script.js
@@ -32,7 +32,7 @@ function loadProblemTickets() {
         MessageProcessor.process({
             serviceId: '/adc-service/rest/v1/services/dashboard_problem_ticket_test/dashboard_problem_ticket_test/dashboard__problem_ticket',
             data: {
-                "limit": -1
+                "limit": 1000000
             },
             success: function (res) {
                 console.log('Response OWS Success:', res);
